2559f8ca6f391325c62bf0832bbcd8ce4dc6dbea
Zhou kai hua <silegon@gmail.com>
2012-04-22 15:57:14 +0800

    fix

--- schema.sql
+++ schema.sql
@@ -1,29 +1,30 @@
-CREATE  TABLE IF NOT EXISTS `silegon`.`tag` (
-      `id_tag` INT NOT NULL AUTO_INCREMENT ,
-      `slug` VARCHAR(20) NOT NULL ,
-      `name` VARCHAR(20) NOT NULL ,
-      `count` INT DEFAULT 1,
-      PRIMARY KEY (`id_tag`) ,
-      UNIQUE INDEX `name_UNIQUE` (`name` ASC) ,
-      UNIQUE INDEX `slug_UNIQUE` (`slug` ASC) 
+CREATE  TABLE IF NOT EXISTS silegon.tag (
+      id_tag INT NOT NULL AUTO_INCREMENT ,
+      slug VARCHAR(20) NOT NULL ,
+      name VARCHAR(20) NOT NULL ,
+      count INT DEFAULT 1,
+      PRIMARY KEY (id_tag) ,
+      UNIQUE INDEX name_UNIQUE (name ASC) ,
+      UNIQUE INDEX slug_UNIQUE (slug ASC) 
 );
 
-CREATE  TABLE IF NOT EXISTS `silegon`.`post` (
-      `id_post` INT NOT NULL AUTO_INCREMENT ,
-      `title` VARCHAR(20) NOT NULL ,
-      `slug` VARCHAR(20) NOT NULL ,
-      `content` TEXT NULL ,
-      `content_html` TEXT NULL ,
-      `content_type` CHAR(1) NULL ,
-      `publish_date` DATE NULL ,
-      PRIMARY KEY (`id_post`) ,
-      UNIQUE INDEX `title_UNIQUE` (`title` ASC) ,
-      UNIQUE INDEX `slug_UNIQUE` (`slug` ASC) 
+CREATE  TABLE IF NOT EXISTS silegon.post (
+      id_post INT NOT NULL AUTO_INCREMENT ,
+      title VARCHAR(45) NOT NULL ,
+      slug VARCHAR(45) NOT NULL ,
+      content TEXT NULL ,
+      content_html TEXT NULL ,
+      content_format CHAR(1) DEFAULT 'R' ,
+      content_status CHAR(1) DEFAULT 'P' ,
+      publish_date DATE NULL ,
+      PRIMARY KEY (id_post) ,
+      UNIQUE INDEX title_UNIQUE (title ASC) ,
+      UNIQUE INDEX slug_UNIQUE (slug ASC) 
 );
 
-CREATE  TABLE IF NOT EXISTS `silegon`.`tag_post` (
-      `id_tag` INT NOT NULL ,
-      `id_post` INT NOT NULL ,
-      UNIQUE INDEX `id_tag_UNIQUE` (`id_tag` ASC) ,
-      UNIQUE INDEX `id_post_UNIQUE` (`id_post` ASC) 
+CREATE  TABLE IF NOT EXISTS silegon.tag_post (
+      id_tag INT NOT NULL ,
+      id_post INT NOT NULL ,
+      UNIQUE INDEX id_tag_UNIQUE (id_tag ASC) ,
+      UNIQUE INDEX id_post_UNIQUE (id_post ASC) 
 );
--- silegon.py
+++ silegon.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 #coding=utf-8
 import MySQLdb
+import datetime
+import markdown
+from docutils.core import publish_parts
+
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort,\
         render_template, flash
@@ -45,9 +49,60 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_index():
-    g.db.execute('select id_tag, slug, name from tag;')
-    tags = [dict(id_tag=row[0], slug=row[1], name=row[2]) for row in g.db.fetchall()]
-    return render_template('index.html', tags=tags)
+    g.db.execute("select title, publish_date, content_html from post where content_status='P';")
+    context = [dict(title=row[0], publish_date=row[1], content_html=row[2])\
+               for row in g.db.fetchall()]
+    return render_template('index.html',c=context)
+
+content_format_dict = {
+    'raw':'R',
+    'rst':'S',
+    'md':'M',
+}
+content_status_dict = {
+    'publish':'P',
+    'draft':'D',
+    'hide':'H',
+}
+@app.route('/new_article', methods=['GET', 'POST'])
+def new_article():
+    error = None
+    if request.method == 'POST':
+        f = request.form
+        title = f.get('title', None)
+        slug = f.get('slug', None)
+        content = f.get('content', None)
+        _format = f.get('format', None)
+        _status = f.get('status', None)
+        format = content_format_dict[_format]
+        if format == 'R':
+            content_html = content
+        elif format == 'S':
+            content_html = publish_parts(content, writer_name='html')['body']
+        elif format == 'M':
+            content_html = markdown.markdown(content)
+        else:
+            abort(401)
+        status = content_status_dict[_status]
+        publish_date = datetime.date.today()
+        g.db.execute("insert into post (title, slug, content, content_html,\
+                     content_format, content_status, publish_date) values \
+                     ('%s','%s','%s','%s','%s','%s','%s')"%\
+                    (title, slug, content, content_html,\
+                     format, status, publish_date)) 
+    context = {
+        'format':'R',
+        'status':'P',
+    }
+    return render_template('article_form.html', c=context, error=error)
+
+
+#@app.route('/edit_article', methods=['GET', 'POST'])
+#def edit_article():
+#    error = None
+#    if request.method == 'POST':
+#        pass
+#    return render_template('article_form.html', error=error)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
